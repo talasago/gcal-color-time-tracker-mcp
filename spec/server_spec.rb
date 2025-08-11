1854c31a45435c31fe8ff72786a88b7f46be7bb4
talasago <42575556+talasago@users.noreply.github.com>
2025-08-11 20:17:23 +0900

    test: add server_spec for CalendarColorMCP::Server

---
+++ spec/server_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+require_relative '../lib/calendar_color_mcp/server'
+
+describe CalendarColorMCP::Server do
+  around do |example|
+    # 環境変数を一時的に保存・復元
+    original_client_id = ENV['GOOGLE_CLIENT_ID']
+    original_client_secret = ENV['GOOGLE_CLIENT_SECRET']
+
+    example.run
+
+    ENV['GOOGLE_CLIENT_ID'] = original_client_id
+    ENV['GOOGLE_CLIENT_SECRET'] = original_client_secret
+  end
+
+  before do
+    # モック設定: 外部依存を分離
+    allow(CalendarColorMCP::TokenManager).to receive(:instance).and_return(double('TokenManager'))
+    allow(CalendarColorMCP::GoogleCalendarAuthManager).to receive(:instance).and_return(double('GoogleCalendarAuthManager'))
+
+    # ログ関連のメソッドをモック化（設計変更予定のため）
+    allow_any_instance_of(CalendarColorMCP::Server).to receive(:setup_logger)
+    allow_any_instance_of(CalendarColorMCP::Server).to receive(:log_info)
+    allow_any_instance_of(CalendarColorMCP::Server).to receive(:log_error)
+    allow_any_instance_of(CalendarColorMCP::Server).to receive(:log_debug)
+  end
+
+  describe '#initialize' do
+    context 'when environment variables are properly set' do
+      before do
+        ENV['GOOGLE_CLIENT_ID'] = 'test_client_id'
+        ENV['GOOGLE_CLIENT_SECRET'] = 'test_client_secret'
+      end
+
+      let(:mock_mcp_server) { double('MCP::Server') }
+
+      before do
+        allow(MCP::Server).to receive(:new).and_return(mock_mcp_server)
+      end
+
+      it 'should not raise an error' do
+        expect { CalendarColorMCP::Server.new }.not_to raise_error
+      end
+    end
+
+    context 'when environment variables are missing' do
+      where(:client_id, :client_secret, :expected_error) do
+        [
+          [nil, 'test_client_secret', /必要な環境変数が設定されていません: GOOGLE_CLIENT_ID/],
+          ['test_client_id', nil, /必要な環境変数が設定されていません: GOOGLE_CLIENT_SECRET/],
+          [nil, nil, /必要な環境変数が設定されていません: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET/],
+          ['', '', /必要な環境変数が設定されていません: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET/]
+        ]
+      end
+
+      with_them do
+        before do
+          ENV['GOOGLE_CLIENT_ID'] = client_id
+          ENV['GOOGLE_CLIENT_SECRET'] = client_secret
+        end
+
+        it 'should raise an error' do
+          expect { CalendarColorMCP::Server.new }.to raise_error(expected_error)
+        end
+      end
+    end
+  end
+end
