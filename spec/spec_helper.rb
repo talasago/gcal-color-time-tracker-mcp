aa3714283b30a812fb73686ad01064688a5a34bd
talasago <42575556+talasago@users.noreply.github.com>
2025-08-12 21:45:22 +0900

    test: set environment variable to suppress logs during test execution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,18 @@
 require 'rspec'
 require 'rspec-parameterized'
 require 'dotenv/load'
+require_relative '../lib/calendar_color_mcp/logger_manager'
 
 RSpec.configure do |config|
+  # テスト環境でのログ抑制用環境変数を設定
+  config.before(:suite) do
+    ENV['RSPEC_RUNNING'] = 'true'
+  end
+  
+  config.after(:suite) do
+    ENV.delete('RSPEC_RUNNING')
+  end
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
